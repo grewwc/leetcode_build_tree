a9edea5d6aa19997254ef4380ee58c6c911da5c9
wwc129 <wangwenchao.wwc@alibaba-inc.com>
2023-07-17 12:29:29 +0800

    remove cppfile

--- leetcode_build_tree.h
+++ leetcode_build_tree.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -19,11 +21,112 @@ struct ListNode
     ListNode(int x) : val(x), next(NULL) {}
 };
 
+// implementations
+namespace
+{
+    TreeNode *build_tree(const std::vector<std::string> &nodes) noexcept
+    {
+        std::vector<TreeNode *> vec(nodes.size());
+        vec[0] = new TreeNode(stoi(nodes[0]));
+        int parent_idx = 0;
+        int count = 0;
+        for (size_t i = 1; i < nodes.size(); i++)
+        {
+            if (count == 2)
+            {
+                count = 0;
+                parent_idx++;
+                while (parent_idx < nodes.size() && !vec[parent_idx]) parent_idx++;
+            }
 
-TreeNode *build_tree(std::string nodes);
+            if (nodes[i] == "null")
+            {
+                count++;
+                continue;
+            }
 
-ListNode* build_list(std::string nodes);
+            vec[i] = new TreeNode(stoi(nodes[i]));
+            if (count % 2 == 1)
+            {
+                vec[parent_idx]->right = vec[i];
+            }
+            else
+            {
+                vec[parent_idx]->left = vec[i];
+            }
+            count++;
+        }
+        return vec[0];
+    }
+} // namespace
 
-void print_tree_preorder(TreeNode *root) noexcept;
+// it is NOT efficient, but does its work
+// we only new, but don't delete (it has memory leak).
+// It doesn't matter in this case.
+// here just copy the string, no string_view or (const) reference
+TreeNode *build_tree(std::string nodes)
+{
+    std::vector<std::string> vec;
 
-void print_list(ListNode* head) noexcept;
+    // transform "," & "[" & "]" to empty space
+    transform(cbegin(nodes), cend(nodes), begin(nodes), [](char ch) {
+        if (ch != ',' && ch != '[' && ch != ']')
+        {
+            return ch;
+        };
+        return ' ';
+    });
+
+    // use iss to ignore empty space
+    std::istringstream iss{move(nodes)};
+    std::string next_word;
+
+    while (!iss.eof())
+    {
+        iss >> next_word;
+        if (!next_word.empty()) // avoid push_back trailing empty space
+            vec.push_back(std::move(next_word));
+    }
+
+    return build_tree(vec);
+}
+ListNode *build_list(string nodes)
+{
+    string node_val;
+    ListNode *head = new ListNode(0);
+    auto cur = head;
+    for (int i = 0; i < nodes.size(); i++)
+    {
+        char ch = nodes[i];
+        switch (ch)
+        {
+        case ' ':
+        case '[': continue;
+        case ',':
+        case ']':
+            cur->next = new ListNode(stoi(node_val));
+            cur = cur->next;
+            node_val.clear();
+            break;
+        default: node_val += ch;
+        }
+    }
+    return head->next;
+}
+
+void print_list(ListNode *head) noexcept
+{
+    while (head)
+    {
+        cout << head->val << endl;
+        head = head->next;
+    }
+}
+
+void print_tree_preorder(TreeNode *root) noexcept
+{
+    if (!root) return;
+    std::cout << root->val << std::endl;
+    print_tree_preorder(root->left);
+    print_tree_preorder(root->right);
+}
